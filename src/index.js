afd4e0ee79d637a236267f38fd85e193f19ea774
James Orte <jamesorte089@gmail.com>
2026-05-22 16:47:48 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -4,18 +4,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const reportWebVitals = (onPerfEntry) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
